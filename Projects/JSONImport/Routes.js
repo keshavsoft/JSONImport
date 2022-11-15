32ca44a91274833211854edbd9c69dec50939115
Keshav <40142486+keshavsoft@users.noreply.github.com>
2022-11-15 15:56:23 +0530

    keshav

--- Projects/JSONImport/Routes.js
+++ Projects/JSONImport/Routes.js
@@ -4,9 +4,11 @@ let CommonMiddlewareJwtVerify = require("../../common/Jwt/Bs5");
 //let CommonApi = require("./Routes/Api");
 let CommonValidate = require('./Routes/Validate');
 let CommonImport = require("./Routes/Import");
+let CommonController = require("../../controllers/JSONImport/jsonimport.controller");
 
 //router.use('/Api', CommonMiddlewareJwtVerify.ForKeshavSoftRedirectToLogin, CommonApi);
 router.use('/Validate', CommonValidate);
 router.use('/Import', CommonMiddlewareJwtVerify.ForKeshavSoftRedirectToLogin, CommonImport);
+router.use('/', CommonController.getUrl);
 
 module.exports = router;
